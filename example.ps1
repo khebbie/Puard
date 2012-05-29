68e42d25ecd4e0fe5e712e00d3f7ede97ebb34e0
Klaus Hebsgaard <khebsgaard@ebay.com>
2012-05-29 22:04:11 +0200

    Initial commi
    This is the first commit

---
+++ example.ps1
@@ -0,0 +1,10 @@
+﻿
+New-Puard jsActions {
+    New-PuardAction "*.js" {echo "hej"} "phantomjs execution"
+    New-PuardAction "*.js" {echo "med"} "minimize"
+}
+
+New-Puard autoptestActions {
+    New-PuardAction "*.test.dll" {echo "dig"} "test changes"
+    New-PuardAction "*.dll" {echo "puard"} "app changes"
+} 
---
+++ puard dsl.ps1
@@ -0,0 +1,33 @@
+﻿function New-Puard {
+    param(
+        [string]$PuardName,
+        [scriptblock]$ScriptBlock
+    )
+
+    function New-PuardAction {
+        param (
+            [string]$Pattern,
+            [scriptblock]$Action,
+            [string]$Name
+        )
+        New-Object PSObject -Property @{
+            PuardName = $PuardName
+            Pattern = $Pattern
+            Name = $Name
+            Action = $Action
+        }
+    }
+    & $ScriptBlock
+}
+cls
+
+
+New-Puard jsActions {
+    New-PuardAction "*.js" {echo "hej"} "phantomjs execution"
+    New-PuardAction "*.js" {echo "med"} "minimize"
+}
+
+New-Puard autoptestActions {
+    New-PuardAction "*.test.dll" {echo "dig"} "test changes"
+    New-PuardAction "*.dll" {echo "puard"} "app changes"
+} 
