5b2b2ed9ad45d80dd1a0bf8b9a82e7544f28a17a
Klaus Hebsgaard <khebsgaard@ebay.com>
2012-05-29 22:37:41 +0200

    mmmm

--- puard dsl.ps1
+++ puard dsl.ps1
@@ -20,14 +20,3 @@
     & $ScriptBlock
 }
 cls
-
-
-New-Puard jsActions {
-    New-PuardAction "*.js" {echo "hej"} "phantomjs execution"
-    New-PuardAction "*.js" {echo "med"} "minimize"
-}
-
-New-Puard autoptestActions {
-    New-PuardAction "*.test.dll" {echo "dig"} "test changes"
-    New-PuardAction "*.dll" {echo "puard"} "app changes"
-} 
