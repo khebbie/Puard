1a6a17e276cc35bf189b7e7ebb63d941cbcf2d0e
Klaus Hebsgaard <khebsgaard@ebay.com>
2012-05-30 22:31:27 +0200

    mm now we are getting somewhere

--- example.ps1
+++ example.ps1
@@ -1,14 +1,17 @@
 ﻿function testing() {
-
+	echo "calling function from action" 
 }
 
+New-Puard csActions {
+    New-PuardAction ".*.cs$" {echo "start building"} "build project"
+}
 
 New-Puard jsActions {
-    New-PuardAction ".*.js" {echo "hej"} "phantomjs execution"
-    New-PuardAction ".*.js" {echo "med"} "minimize"
+    New-PuardAction ".*.js$" {echo "hej"} "phantomjs execution"
+    New-PuardAction ".*.js$" { testing } "minimize"
 }
 
 New-Puard autoptestActions {
-    New-PuardAction ".*.test.dll" {echo "dig"} "test changes"
-    New-PuardAction ".*.dll" {echo "puard"} "app changes"
+    New-PuardAction ".*.test.dll$" {echo "dig"} "test changes"
+    New-PuardAction ".*.dll$" {echo "puard"} "app changes"
 } 
