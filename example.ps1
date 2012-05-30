6a4e45cb501f0b1fac6c4c6b6f8d67e2aca967af
Klaus Hebsgaard <khebsgaard@ebay.com>
2012-05-30 16:09:55 +0200

    m'

--- example.ps1
+++ example.ps1
@@ -1,10 +1,14 @@
-﻿
+﻿function testing() {
+
+}
+
+
 New-Puard jsActions {
-    New-PuardAction "*.js" {echo "hej"} "phantomjs execution"
-    New-PuardAction "*.js" {echo "med"} "minimize"
+    New-PuardAction ".*.js" {echo "hej"} "phantomjs execution"
+    New-PuardAction ".*.js" {echo "med"} "minimize"
 }
 
 New-Puard autoptestActions {
-    New-PuardAction "*.test.dll" {echo "dig"} "test changes"
-    New-PuardAction "*.dll" {echo "puard"} "app changes"
+    New-PuardAction ".*.test.dll" {echo "dig"} "test changes"
+    New-PuardAction ".*.dll" {echo "puard"} "app changes"
 } 
